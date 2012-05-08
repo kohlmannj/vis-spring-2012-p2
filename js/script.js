b3692cb6b6be6f3962c451bd1548a123141061e5
Joe Kohlmann <kohlmannj@gmail.com>
2012-05-07 19:54:06 -0500

    I have centroid circles on the map. Not every one, though. Hmm.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,63 @@
 var po = org.polymaps;
 
+var kmlTypes = {
+
+  Point: function(e) {
+    var json = {
+      type: "Point",
+      coordinates: e.getElementsByTagName("coordinates")[0]
+        .textContent
+        .split(",")
+        .map(Number)
+    };
+    
+    return json.coordinates;
+  },
+
+  LineString: function(e) {
+    var json = {
+      type: "LineString",
+      coordinates: e.getElementsByTagName("coordinates")[0]
+        .textContent
+        .trim()
+        .split(/\s+/)
+        .map(function(a) { return a.split(",").slice(0, 2).map(Number); })
+    };
+    
+    return json.coordinates;
+  },
+  
+  Polygon: function(e) {
+    var json = {
+      type: "Polygon",
+      coordinates: [e.getElementsByTagName("outerBoundaryIs")[0].getElementsByTagName("LinearRing")[0].getElementsByTagName("coordinates")[0]
+      .textContent
+      .trim()
+      .split(/\s+/)
+      .map(function(a) { return a.split(",").slice(0, 2).map(Number); })]
+    }
+    
+    return json.coordinates;
+  }
+
+};
+
+function kmlGeometry(e) {
+  return e && e.tagName in kmlTypes && kmlTypes[e.tagName](e);
+}
+
+function kmlGeoJson(e) {
+  // var f = {id: e.getAttribute("id"), properties: {}};
+  for (var c = e.firstChild; c; c = c.nextSibling) {
+    var g = kmlGeometry(c);
+    if (g) {
+      return g;
+    }
+  }
+  return [];
+  // return {type: "FeatureCollection", features: features};
+}
+
 // Create the map object, add it to #map…
 var map = po.map()
     .container(d3.select("#content").append("svg:svg").node())
@@ -27,7 +85,7 @@ d3.xml("data/?src=locations&by=kml", function(xmlResult) {
     var folders = d3.select(xmlResult).selectAll("Folder")[0];
     
     var addLocindexasID = function(d, i) {
-        console.log("loc" + d3.select(d).attr("index")); return "loc" + d3.select(d).attr("index");
+        return "loc" + d3.select(d).attr("index");
     };
     
     var group = layer.selectAll("g").data(folders)
@@ -39,8 +97,47 @@ d3.xml("data/?src=locations&by=kml", function(xmlResult) {
         var selection = d3.select(d).select("Placemark")[0];
         return selection;
     }).enter().append("g").each(function(d) {
-        console.log(d3.select(d).attr("index"), d3.select(d).select("name")[0]);
+        // console.log(d3.select(d).attr("index"), d3.select(d).select("name")[0]);
+    }).attr("r", 4.5).attr("transform", function(d, i) {
+        // Find the centroid of all the coordinates.
+        var coordinates = kmlGeoJson(d);
+        console.log(coordinates);
+        
+        d.centroid = {
+            lon: 0,
+            lat: 0
+        };
+        
+        for (var i = 0; i < coordinates.length; i++) {
+          
+            var item = coordinates[i];
+          
+            var tempCentroid = {
+                lon: 0,
+                lat: 0
+            };
+          
+            for (var j = 0; j < item.length; j++) {
+                tempCentroid.lon = tempCentroid.lon + item[j][0];
+                tempCentroid.lat = tempCentroid.lat + item[j][1];
+            }
+            
+            d.centroid.lon = d.centroid.lon + tempCentroid.lon / item.length;
+            d.centroid.lat = d.centroid.lat + tempCentroid.lat / item.length;
+        }
+        
+        d.centroid.lon = d.centroid.lon / coordinates.length;
+        d.centroid.lat = d.centroid.lat / coordinates.length;
+        
+        console.log(d.centroid);
+        
+        d = map.locationPoint(d.centroid);
+        return "translate(" + d.x + "," + d.y + ")";
+    }).attr("id", addLocindexasID).attr("title", function(d, i) {
+        return d3.select(d).select("name").text();
     });
+    
+    loc.append("svg:circle").attr("r", 12);
     
     // d3.select(d).selectAll("Placemark")
     
@@ -50,24 +147,24 @@ d3.xml("data/?src=locations&by=kml", function(xmlResult) {
     //     return "0";
     // });
     
-    loc.append("svg:circle").attr("r", 4.5);
+    // loc.append("svg:circle").attr("r", 4.5);
     
-    // map.on("move", function() {
-    //     layer.selectAll("g").attr("transform", function(d) {
-    //         d = map.locationPoint({lon: d.value[0], lat: d.value[1]});
-    //         return "translate(" + d.x + "," + d.y + ")";
-    //     });
-    // });
+    map.on("move", function() {
+        group.selectAll("g").attr("transform", function(d) {
+            d = map.locationPoint(d.centroid);
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
 });
 
 map.add(po.compass()
     .pan("none"));
 
-function load(e) {
-  for (var i = 0; i < e.features.length; i++) {
-    var f = e.features[i], p = f.data.properties;
-    f.element.appendChild(po.svg("title").appendChild(
-        document.createTextNode(p.name + ": " + p.description))
-        .parentNode);
-  }
-}
+// function load(e) {
+//   for (var i = 0; i < e.features.length; i++) {
+//     var f = e.features[i], p = f.data.properties;
+//     f.element.appendChild(po.svg("title").appendChild(
+//         document.createTextNode(p.name + ": " + p.description))
+//         .parentNode);
+//   }
+// }
